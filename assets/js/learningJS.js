d5878b65f1f15b896edffed4e5a2f08250ec820b
Nelson Edohore Francis <mrnelsonfrancis@gmail.com>
2022-09-12 16:43:09 +0100

    initial commit

---
+++ assets/js/learningJS.js
@@ -0,0 +1,817 @@
+// alert("hello");
+
+
+// function plus(a, b){
+//     return a + b ;
+// }
+
+// alert(plus(10, 10))
+// document.getElementById("love").innerHTML="fuck your skull bambi"
+
+// console.log(prompt("what is your name" , "jhon doe:" ));
+// console.log(confirm("still learning"));
+// console.log("hey there im doing something");
+// console.warn("im warning you")
+// console.error("error retry bitch")
+// alertabc("this is an error")
+// console.log(
+//     "%c hey there im doing something", 
+//     "color:blue; font-size:20px; font-weight:bold;")
+// alert("js is cool");
+// console.log(prompt("what country are you from" , "nigeria"))
+// console.log(confirm("im still learing js"))
+// console.log("im learning js")
+
+// let carTypes;
+// carTypes = "nelson";
+// console.log("carTypes");
+// let carTypes = "toyota ";
+// let drinks = "fanta";
+// let year = 2000;
+// let firstname = "nelson";
+// firstname = "miracle";
+// console.log(firstname);
+// const lastname = "young"
+// console.log(lastname)
+// let favFood = "pizza"
+// let favColor = "red"
+// let favDrink = "lemonade"
+// console.log(favDrink)
+// favFood = "rice"
+// console.log(favFood)
+// let myBirthday = 14 
+// console.log(myBirthday)
+
+
+// let num = 7
+// let newNum = 7.58
+
+// let name = "nelson"
+
+// console.log(1000>100)
+
+// let js = "cool"
+// console.log(js == "cool")
+
+// let bigNumber = 1000000000000000000000n
+// console.log (typeof bigNumber)
+
+// let cahnnelName;
+// console.log (typeof cahnnelName)
+
+// let carName = ["nelson", false , 4]
+// console.log(carName[1])
+
+// let fName = function () {
+//     console.log("fName");
+// }
+// fName ();
+
+// let gender = {
+//     first: "male",
+//     second: "female",
+//     third : "homo",
+//     fourth : "lesbian",
+//     number : 5,
+// }
+// console.log(gender.second)
+
+
+
+// let food = ["yam", 7 , "rice", {
+//     first: "male",
+//     second: "female",
+//     third : "homo",
+//     fourth : "lesbian",
+//     number : 5,
+// }, ]
+// console.log(food[3]);
+// console.log(food[4]);
+
+// let rice = "seawater"
+// let beans = "stream water"
+// function name() {
+//     console.log(rice, beans)
+// }
+// name();
+
+// let name = function(){
+//     console.log(rice, beans)
+// }
+// name()
+
+// let name = () => console.log(rice, beans)
+// name()
+
+
+
+// let firstName = "nelson"
+// let lastName = "francis"
+// let age = 18
+
+// let pStatus = () => console.log(firstName, lastName,age);
+// pStatus()
+
+// let gender = {
+//     first : "male",
+//     second : "female",
+//     third : "rather not say"
+// }
+// console.log(gender.third)
+
+// let everything = [{
+//     first : "male",
+//     second : "female",
+//     third : "rather not say"
+// }, "name", "state" , "country","() => console.log(firstName, lastName,age)"]
+// console.log(everything[0])
+// console.log(everything[3])
+// console.log(everything[4])
+
+// console.log(5+2);
+// console.log(5-2);
+// console.log(5/2);
+// console.log(5*2);
+// console.log(5%2);
+// console.log(5**2); // 5x5
+// console.log(5**3);
+
+// let score = 99;
+// score--;
+// score++;
+// console.log(score)
+
+
+// let sum = 5 + 3 * 2 - 1 * (1 + 1)
+// console.log(sum)
+
+
+// let num1 = 10
+// let num2 = 20
+//  console.log(num1 + num2)
+
+//  console.log(num1%num2)
+  
+//  let total = 10
+//  total++
+//  console.log(total)
+
+// let total = () => console.log(num1 + num2);
+// total()
+
+
+// let school = " mr soft "
+// console.log(school)
+
+
+// += operator
+// let total = 50
+// total = total + 50
+// total += 50
+// console.log(total);
+
+// -= operator
+// let balance = 450
+// balance -= 400
+// balance = balance - 400
+// console.log(balance);
+
+// *= operator
+//  let donation = 100
+//  donation *= 100
+// donation = donation * 100
+//  console.log(donation);
+
+//  /= operator
+//  let sweet = 5000
+//  sweet /= 200
+//  console.log(sweet);
+
+//  %= operator
+//  let yam = 10
+//  yam %= 2
+//  console.log(yam)
+
+//  let x = 30
+//  x += 10
+//  x /= 2
+//  console.log(x);
+
+//  let xp = 250
+//  xp *= 2
+//  console.log(xp);
+
+
+// let num = 3
+// let newNum = 3.16
+
+// let mill = 5e3
+// console.log(mill);
+
+// let smallNum = 5e-3
+// console.log(smallNum);
+
+// // let score = new Number (100)
+// console.log(newNum.toFixed());
+
+
+
+// let theVariable = 1e6
+// console.log(theVariable);
+// console.log(typeof theVariable)
+// let floatData = 1e-6
+// console.log(floatData);
+
+
+
+// let num = 1e5
+// console.log (num)
+// console.log(typeof num);
+
+// let s = 'single'
+// let d = "double" 
+// let b = `backtick`
+
+// let speech = "martin luther king said 'i have a dream ' "
+// console.log(speech);
+
+// let tip = "we can escape quotes by using /"//" "
+// console.log(tip)
+
+// let festAns = ' my name is '
+// let firstWord = prompt("what is your name ?")
+// let ageA = " , i am "
+// let age = prompt("how old are you ?")
+// let digit = " years old"
+// let secndAns = ' and this is my first js lesson'
+
+// let combo = festAns.concat(firstWord).concat(secndAns)
+
+
+// console.log("Hello" + combo );
+
+// let vari1 = "i am learning "
+// let vari2 = "javaScript"
+// // let answer = vari1 + vari2
+// let answer = vari1.concat(vari2)
+// console.log(answer);
+
+// let firstName = `nelson`
+// let course = `javascript`
+// let channel = `devdreamer`
+// let info = ` ${firstName} is learning ${course} with ${channel} .` ;
+// console.log(info);
+
+
+// let x = "i am watching dev dreamer"
+// console.log(x.toLocaleUpperCase());
+// console.log(x.slice(14,25));
+
+// let currentDate = new Date()
+// console.log(
+//   currentDate.toLocaleString('defult', {
+//     weekday:"long",
+//     month:"short",
+//     year:"numeric"
+//   })
+// );
+
+// let date = new Date()
+// let day = date.toLocaleString('default', {weekday:"short"})
+// let year = date.getFullYear()
+// let hour = date.getHours()
+// let min = date.getMinutes()
+// let sec = date.getSeconds()
+// let mill = date.getMilliseconds()
+
+// console.log(`${date.getDate()}-${day}-${year} ${hour}:${min}:${sec}:${mill}`);
+
+
+// let num1 = Math.random().toFixed(1) * 10
+// let num2 = Math.random().toFixed(1) * 10
+// let num3 = Math.random().toFixed(1) * 10
+ 
+// console.log(`${num1}-${num2}-${num3}`);
+
+// let max = Math.min(num1 , num2 , num3)
+// console.log(max);
+
+//  let x = 7
+//  console.log(x > 20 , x > 2);
+
+//  console.log(
+//     x >= 10 , x >= 7
+//  );
+
+//  console.log(
+//     x < 10 
+//  );
+
+//  console.log(
+//     x <= 10 
+//  );
+
+//  console.log(
+//     x == 10 , x == 7
+//  );
+
+//  console.log(
+//     x != 10 , x != 7
+//  );
+
+// let y = "7"
+
+// console.log(
+//     x === 7 , x === y 
+// );
+
+// console.log(
+//     x !==7 , x !== y
+// );
+
+// console.log(
+//     x == y , x != y
+// );
+
+// console.log(
+//     'A' > 'Z' ,  'a' < 'z' ,  'A' > 'z'
+// );
+
+// let a = 'life'
+// let b = 'beat'
+
+// console.log(
+//  a < b
+// );
+
+
+
+
+
+// let a = 10
+// let b = 15
+
+// console.log(
+//     a > b 
+// );
+
+// console.log(
+//     a == "10 "
+// );
+
+// console.log(
+//     a === "10"
+// );
+
+// let x = "5" 
+// let y = 6 
+
+// console.log(
+//     x !== y
+// );
+
+
+//  let x = 1
+//  let y = 0
+ 
+
+//  if (x && y) {
+//     console.log("hello");
+//  }
+//  if (x && !y) {
+//     console.log("hello");
+//  }
+
+// if (x || y ) {
+//     console.log("hello");
+// }
+
+// x || alert("this is ignored since x is true")
+// y || alert("this is alerted since y is false")
+ 
+
+// let a = "Dev Dreamer"
+// let b = ''
+
+
+// if (a || b) {
+//     console.log("hello");
+// }
+
+
+// if (a && true && !b) {
+//     console.log("hello"); 
+// }
+
+// a = -1
+// b = 1 
+
+// if (a && b) {
+//     console.log("nice you too good LORD NELSON the king of the whole world");
+// }
+
+// let score = 0
+
+// console.log(score ?? 'no score registered');
+
+// let course
+// console.log(course ?? "please select a course" );
+
+// let courseProgress = 0
+// console.log(
+//     courseProgress ?? "start the course"
+// );
+
+// let game = 'sonic'
+
+
+// if (game === 'sonic' ) {
+//     console.log("the condition is true");
+// } else {
+//     console.log('game is not = sonic');
+// }
+
+// let roll = Math.random().toFixed(1) * 10
+// if (roll === 4) {
+//     console.log('you rolled a one');
+// } else if (roll === 5) {
+//     console.log('you rolled a five');
+// } else if (roll === 6) {
+//     console.log('you rolled a six');
+// }else{
+//     console.log('you rolled less that 4');
+// }
+
+// let num = 10 + 2
+// if (num > 2 && num < 20) {
+//     console.log('true');
+// } else {
+//     console.log('false');
+// }
+
+// let user = 'employee'
+// if (user === 'guest') {
+//     console.log('login denied');
+// }else if (user === 'employee'){
+// console.log('successfully logged in');
+// }
+
+// let myName = 'nelson'
+// let nameLength = myName.length
+// if (nameLength > 5) {
+//    alert('more than five')
+// }else if (nameLength === 5){
+//     alert('exactly five letters')
+// }else{
+//     alert('less than five letters')
+// }
+
+// let roll = Math.random().toFixed(1) * 10
+// switch (roll) {
+//     case 1:
+//     console.log('you rolled a 1');
+//     break;
+//     case 2:
+//     console.log('you rolled a 2');
+//     break;
+//     case 3:
+//     console.log('you rolled a 3');
+//     break;
+//     case 4:
+//     console.log('you rolled a 4');
+//     break;
+//     case 5:
+//     console.log('you rolled a 5');
+//     break;
+//     case 6:
+//     console.log('you rolled a 6');
+//     break;
+//     default:
+//         console.log(`the number ${roll} is not possible`);
+// }
+
+// let today = new Date().toLocaleString('default' , {month: 'long'})
+// switch (true) {
+//     case today == 'january':
+//     console.log(`toady is ${today}`);
+//     break;
+//     case today == 'frebruary':
+//     console.log(`toady is ${today}`);
+//     break;
+//     case today == 'march':
+//     console.log(`toady is ${today} `);
+//     break;
+//     case today == 'april':
+//     console.log(`toady is ${today}`);
+//     break;
+//     case today == 'may':
+//     console.log(`toady is ${today}`);
+//     break;
+//     case today == 'june':
+//     console.log(`toady is ${today}`);
+//     break;
+//     case today == 'august':
+//     console.log(`toady is ${today}`);
+//     break;
+//     case today == 'september':
+//     console.log(`toady is ${today}`);
+//     break;
+//     case today == 'october':
+//     console.log(`toady is ${today}`);
+//     break;
+//     case today == 'november':
+//     console.log(`toady is ${today}`);
+//     break;
+//     default:
+//         console.log(`i dont know what today is `);
+        
+// }
+
+// let favFood = 'rice'
+// switch (favFood) {
+//     case  'yam':
+//         console.log(`thats right ${favFood} is my favourite`);
+//         break;
+//     case 'beans':
+//         console.log(`thats right ${favFood} is my favourite`);
+//         break;
+//     case  'plantain':
+//         console.log(`thats right ${favFood} is my favourite`);
+//         break;
+//     case  'rice':
+//         console.log(`thats right ${favFood} is my favourite`);
+//         break;
+//     case  'egg':
+//         console.log(`thats right ${favFood} is my favourite`);
+//         break;
+
+//     default:
+//         console.log(`${favFood} not found...I'm hungry `);
+//         break;
+// }
+
+// let age = 18 
+// if (age >= 18 ) {
+//     console.log('you can vote');
+// } else {
+//     console.log('sorry you cant vote');
+// }
+
+// let message = age >= 18 ? 'you can vote' : 'you cant vote';
+// console.log(message);
+
+
+
+// let message = ''
+
+// let answer = 1 > 10 ? 'condition is TRUE' : 'condition is FALSE'
+ 
+// console.log(answer);
+
+// let time = '12:00'
+
+// let report = time === '12:00' ? 'good morning' : 'good evening'
+// console.log(report);
+
+// let greeting = time < '12:00' ? 'good morning' : 'good evening'
+// console.log(greeting);
+
+
+// for (let i = 0; i <= 10 ; i++) {
+//     console.log(i);
+    
+//  }
+// const characters = ['sonic', 'tails', 'mario', 'luigi'];
+
+// let para = document.querySelector('p');
+// let info = ` the characters are ` ;
+
+// for (let i= 0; i < characters.length; i++ ) {
+//     if (i === characters.length -1) {
+//         info += `and ${characters[i]}`
+//     } else {
+//         info += `${characters[i]}, `
+//     }
+    
+// }
+// para.innerText = info
+
+
+// const colors = ['blue', 'red', 'yellow', 'pink', 'green']
+// for(let color = 0; color < colors.length ; color++ ){
+//     console.log(`${colors[color]} is my favourite color`);
+// }
+
+// let i = 0
+// while (i<=10) {
+//     console.log(i);
+//     i++
+// }
+
+// const colors = ['red' , 'blue' , 'green' , 'orange' , 'purple' ]
+
+// let color = 0
+// while (color < colors.length ) {
+//     console.log(`${colors[color]}`);
+//     color++
+    
+// }
+
+// let i = 0
+// do {
+//     console.log(`the number is ${i}`);
+//     i++
+// } while (i <= 10);
+
+// let character = {
+//     name:'sonic' ,
+//     color: ' blue' ,
+//     speed : 1000 ,
+// }
+
+// for(let x in character){
+//     console.log(`${x} : ${character[x]} `);
+// }
+
+// const salaries = {
+//     peter : 24000, 
+//     james : 34000,
+//     jhon : 55000,
+// }
+
+// for(let i in salaries){
+//     let salary = `$${salaries[i]}`
+//     console.log(`${i} earns ${salary} per year`);
+// }
+
+// character.name = 'knuckles'
+// delete character.speed
+// character.rate = 'nice'
+
+// Object.defineProperty(character, 'name' , {
+//     value : 'tails',
+//     enumerable : true,
+//     configurable : false,
+//     writable : false,
+// })
+// console.log(character)
+// for(x in character){
+//     console.log(`${x} : ${character[x]}`);
+// }
+ 
+// let house = {
+//    type : 'detached',    
+//    build :'brick',
+//     year:2019,
+//     price:250000,
+//     driveway:true,
+// }
+
+// for(let x in house ){
+//     console.log(`${x} : ${house[x]}`);
+// }
+
+// let characters = ['sonic', 'tails', 'mario', 'luigi', 'link']
+
+// for (x of characters){
+//     if (x === 'luigi'){
+//         break
+//     }else{
+//         console.log(x);
+//     }
+// }
+
+// let course = " javascript"
+// for(letter of course){
+//     console.log(letter);
+// }
+
+
+// let colours = ['red', 'orange', 'yellow', 'green', 'blue']
+// for(x of colours){
+//     if (x === 'green') {
+//         break
+//     } else {
+//         console.log(x);
+//     }
+// }
+
+// let supers = ['superman', 'batman', 'flash', 'aquaman',1, 'hello', true, false, ['good', 'evil', 'neutral']]
+// supers[4]= 'wonder woman'
+// delete supers[2]
+// supers[0]= 'yam'
+// console.log(supers[8][2]);
+
+// let countries = ['nigeria', 'america', 'canada']
+// countries[3]='romania'
+// console.log(countries);
+
+// let colours = ['yellow', 'blue', 'green', ['purple','indigo', 'grey']]
+// console.log(colours[3][2]);
+
+
+let supers = ['superman', 'batman', 'flash', 'aquaman']
+
+console.log(supers[supers.length - 1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ assets/js/project.js
@@ -0,0 +1,68 @@
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
+for (let i = 11; i > 10 ; i++) {
+    console.log(i);
+    
+ }
